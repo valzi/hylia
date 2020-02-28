afd050865103ade8dd14d2d829158ed533da4879
valzi <valzibacktrack@gmail.com>
2020-02-27 20:06:48 -0600

    webmentions js and filters js
    both for adding webmentions https://mxb.dev/blog/using-webmentions-on-static-sites/#webmentions

---
+++ src/_data/filters.js
@@ -0,0 +1,23 @@
+const sanitizeHTML = require('sanitize-html')
+
+function getWebmentionsForUrl(webmentions, url) {
+    const allowedTypes = ['mention-of', 'in-reply-to']
+
+    const hasRequiredFields = entry => {
+        const { author, published, content } = entry
+        return author.name && published && content
+    }
+    const sanitize = entry => {
+        const { content } = entry
+        if (content['content-type'] === 'text/html') {
+            content.value = sanitizeHTML(content.value)
+        }
+        return entry
+    }
+
+    return webmentions
+        .filter(entry => entry['wm-target'] === url)
+        .filter(entry => allowedTypes.includes(entry['wm-property']))
+        .filter(hasRequiredFields)
+        .map(sanitize)
+}
